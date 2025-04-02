343014eeccae899f8ec2a6183d8b6df8d3d38944
Atom-gnomov <gorbach0864213579@gmail.com>
2025-04-02 20:03:12 +0300

    Complete

--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from taxi.models import Driver, Car, Manufacturer
 
@@ -13,3 +14,38 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    context_object_name = "manufacturer_list"  # Updated
+    paginate_by = 5
+    queryset = Manufacturer.objects.order_by("name")
+    template_name = "taxi/manufacturer_list.html"
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    context_object_name = "driver_list"  # Updated
+    paginate_by = 5
+    queryset = Driver.objects.order_by("is_active")
+    template_name = "taxi/driver_list.html"
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    template_name = "taxi/driver_detail.html"
+
+
+class CarListView(generic.ListView):
+    model = Car
+    context_object_name = "car_list"  # Updated
+    paginate_by = 5
+    queryset = Car.objects.select_related("manufacturer")
+    template_name = "taxi/car_list.html"
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
+    context_object_name = "car"
+    template_name = "taxi/car_detail.html"

cb46ede5e4e26d87233902efd3306537581b8d69
Atom-gnomov <gorbach0864213579@gmail.com>
2025-04-03 11:58:30 +0300

    Complete

--- taxi/urls.py
+++ taxi/urls.py
@@ -6,10 +6,12 @@ from .views import (index, ManufacturerListView,
 
 urlpatterns = [
     path("", index, name="index"),
-    path("manufactures", ManufacturerListView.as_view(), name="manufacturer-list"),
+    path("manufactures", ManufacturerListView.as_view(),
+         name="manufacturer-list"),
     path("drivers", DriverListView.as_view(), name="driver-list"),
     path("cars", CarListView.as_view(), name="car-list"),
-    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
+    path("drivers/<int:pk>/", DriverDetailView.as_view(),
+         name="driver-detail"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
 ]
 
